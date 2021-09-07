c6abd06965ed3b0c5f8c71f7178149564ee35b71
Mahendra Prasad Gupta <mpgupta1993@gmail.com>
2021-09-08 02:22:08 +0530

    featured youtuber and onboard youtuber render dynamically from backend

--- tubers/youtubers/admin.py
+++ tubers/youtubers/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from . models import Youtuber
+from django.utils.html import format_html
+
 
 # Register your models here.
+class YoutuberAdmin(admin.ModelAdmin):
+
+    def myphoto(self,object):
+        return format_html('<img src="{}" width="40px"/>'.format(object.photo.url))
+
+
+    list_display = ('id','name','category','created_date','myphoto','is_featured')
+    list_display_links = ('name',)
+    search_fields = ('name','category')
+    list_filter = ('category',)
+    list_editable = ('is_featured',)
+
+admin.site.register(Youtuber,YoutuberAdmin)
--- tubers/youtubers/models.py
+++ tubers/youtubers/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from datetime import datetime
+from ckeditor.fields import RichTextField
 
 # Create your models here.
+
+class Youtuber(models.Model):
+
+    crew_choices =(
+        ('solo','solo'),
+        ('small','small'),
+        ('large','large'),
+    )
+
+    camera_choices =(
+        ('canon','canon'),
+        ('nikon','nikon'),
+        ('sony','sony'),
+        ('Red','Red'),
+        ('fuji','fuji'),
+        ('panasonic','panasonic'),
+        ('others','others'),
+    )
+
+    catagory_choices =(
+        ('code','code'),
+        ('cooking','cooking'),
+        ('mobile_review','mobile_review'),
+        ('vlogs','vlogs'),
+        ('comedy','comedy'),
+        ('gaming','gaming'),
+        ('film_making','film_making'),
+    )
+
+
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    photo = models.ImageField(upload_to='media/ytubers/%Y/%m')
+    video_url = models.CharField(max_length=255)
+    description = RichTextField()
+    city = models.CharField(max_length=255)
+    age = models.IntegerField()
+    height = models.IntegerField()
+    crew = models.CharField(choices=crew_choices,max_length=255)
+    camera_type = models.CharField(choices=camera_choices,max_length=255)
+    sub_count = models.CharField(max_length=255)
+    category = models.CharField(choices=catagory_choices,max_length=255)
+    is_featured = models.BooleanField(default=False)
+    created_date = models.DateTimeField(default=datetime.now,blank=True)
+
