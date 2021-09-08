ab54bd57036a982c94dec2f719d0466f101640db
Mahendra Prasad Gupta <mpgupta1993@gmail.com>
2021-09-08 23:55:56 +0530

    tuber details page

--- tubers/youtubers/views.py
+++ tubers/youtubers/views.py
@@ -1,12 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from youtubers.models import Youtuber
 
 # Create your views here.
 
 def youtubers(request):
-    pass
+    youtubers = Youtuber.objects.order_by("-created_date")
+    data={
+        'youtubers':youtubers
+    }
+    return render(request,'youtubers/tubers.html',data)
 
 def youtubers_detail(request,id):
-    pass
+    tuber= get_object_or_404(Youtuber,pk=id)
+    data={
+            'tuber':tuber,
+        }
+    return render(request,'youtubers/tuberdetails.html',data)
 
 def search(request):
     pass
