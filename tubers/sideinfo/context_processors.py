c486a6e80642c6393883ff011ac157283d82b72f
Mahendra Prasad Gupta <mpgupta1993@gmail.com>
2021-09-11 23:39:47 +0530

    pagination functionality activated

--- tubers/sideinfo/context_processors.py
+++ tubers/sideinfo/context_processors.py
@@ -3,7 +3,9 @@ from . models import HeaderSideinfo,FooterSideinfo
 def extras(request):
 
     headersideinfo = HeaderSideinfo.objects.order_by('-created_date').filter(active = True)[0]
+    footersideinfo = FooterSideinfo.objects.order_by('-created_date').filter(active = True)[0]
     data={
         'headersideinfo':headersideinfo,
+        'footersideinfo':footersideinfo,
     }
     return data
--- tubers/youtubers/views.py
+++ tubers/youtubers/views.py
@@ -1,31 +1,37 @@
 from django.shortcuts import get_object_or_404, render
 from youtubers.models import Youtuber
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 # Create your views here.
 
 def youtubers(request):
-    youtubers = Youtuber.objects.order_by("-created_date")
+    
+    youtubers = Youtuber.objects.order_by("-created_date").all()
+    p = Paginator(youtubers, 2)  
+    page_number = request.GET.get('page')
+
+    try:
+        page_obj = p.get_page(page_number)
+    except PageNotAnInteger:
+
+        page_obj = p.page(1)
+    except EmptyPage:
+       
+        page_obj = p.page(p.num_pages)
+
+
+
     city_search = Youtuber.objects.values_list('city', flat=True).distinct()
     camera_search = Youtuber.objects.values_list('camera_type', flat=True).distinct()
     category_search = Youtuber.objects.values_list('category', flat=True).distinct()
+
     
-    if 'city' in request.GET:
-        city = request.GET['city']
-        if city:
-            youtubers = youtubers.filter(city__iexact=city)
-    if 'camera' in request.GET:
-        camera = request.GET['camera']
-        if camera:
-            youtubers = youtubers.filter(camera_type__iexact=camera)
-    if 'category' in request.GET:
-        category = request.GET['category']
-        if category:
-            youtubers = youtubers.filter(category__iexact=category)
     data={
-        'youtubers':youtubers,
+        'page_obj':page_obj,
         'city_search':city_search,
         'camera_search':camera_search,
         'category_search':category_search,
+    
     }
     return render(request,'youtubers/tubers.html',data)
 
